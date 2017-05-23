fc50d4b1886ce2a00a4c5a7dc019833d98a4aebf
Christopher Williams <chris.a.williams@gmail.com>
2017-05-23 11:09:39 -0400

    Bump to V8 5.8.283.32, use 5.8 patch on builds, enable inspector (#21)

--- build_mac_v8.sh
+++ build_mac_v8.sh
@@ -2,7 +2,7 @@ git clone https://chromium.googlesource.com/chromium/tools/depot_tools.git
 export PATH=`pwd`/depot_tools:"$PATH"
 git apply 0000-hack-gclient-for-travis.patch
 cd v8
-git apply ../ndk14_5.7.patch
+git apply ../ndk14_5.8.patch
 gclient sync
 cd ..
 wget http://dl.google.com/android/repository/android-ndk-r14b-darwin-x86_64.zip
