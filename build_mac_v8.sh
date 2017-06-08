4b770f94f68175ef71eea10debc0c1fa7f2ef6c9
Christopher Williams <chris.a.williams@gmail.com>
2017-06-08 11:45:50 -0400

    Update local mac build script

--- build_mac_v8.sh
+++ build_mac_v8.sh
@@ -1,20 +1,28 @@
+#!/bin/sh
 if [ -d "depot_tools" ]; then
+  echo "Updating existing depot_tools checkout..."
   cd depot_tools
-	git pull origin master
+  git pull origin master
   cd ..
 else
-	git clone https://chromium.googlesource.com/chromium/tools/depot_tools.git
+  echo "Cloning depot_tools..."
+  git clone https://chromium.googlesource.com/chromium/tools/depot_tools.git
 fi
 
 export PATH=`pwd`/depot_tools:"$PATH"
 cd v8
 git checkout -- . # "clean" the v8 directory
+rm -rf out/
 git apply ../ndk11c_5.9.patch
-gclient sync --shallow --no-history --reset --force
+echo "Asking gclient to update v8 dependencies"
+gclient sync --shallow --no-history
 cd ..
 # wget http://dl.google.com/android/repository/android-ndk-r11c-darwin-x86_64.zip
 # unzip android-ndk-r11c-darwin-x86_64.zip
 # export ANDROID_NDK=${PWD}/android-ndk-r11c
-./build_v8.sh -j8 -l ia32 -m release
-./build_v8.sh -j8 -l arm -m release
-./build_v8.sh -t -m release
+echo "Building v8 for x86..."
+./build_v8.sh "-j" "8" "-l" "ia32" "-m" "release"
+echo "Building v8 for ARM..."
+./build_v8.sh "-j" "8" "-l" "arm" "-m" "release"
+echo "Packaging built v8 into tarball..."
+./build_v8.sh "-t" "-m" "release"
