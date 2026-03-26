8276472ac9a4bdb27188625da2b21f49b3ae3ca5
m1ga <migamiga@web.de>
2026-03-26 12:04:42 +0100

    update workflow

--- build_v8.sh
+++ build_v8.sh
@@ -194,6 +194,7 @@ android_ndk_root = "$NDK_DIR"
 host_os = "linux"
 clang_base_path = "$CLANG_BASE"
 is_clang = true
+clang_use_chrome_plugins = false
 
 # Disable v8 internal plugins that require special clang
 v8_enable_verify_heap = false
